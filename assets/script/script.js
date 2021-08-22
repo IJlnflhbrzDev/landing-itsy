0f75eb6e5646e6e1e7ae0a3059d86c906aa8bd6e
unknown <ijlnflhbrzdev@gmail.com>
2021-08-22 17:12:35 +0700

    xample darkmode mengunakan filter: invert

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,3 +4,23 @@ window.addEventListener('scroll', function () {
      let window_position = window.scrollY > 0;
      navbar.classList.toggle('change_scroll_navbar_bg', window_position);
 });
+
+const mode = document.querySelector(".tombol-darkmode");
+// mode.getAttributeNames()
+// let darkmode = mode.getAttribute("data-mode");
+// mode.addEventListener("click", function () {
+//      if (darkmode == "darkmode") {
+//           mode.setAttribute("data-mode", "lightmode")
+//      } if (darkmode == "lightmode") {
+//           mode.setAttribute("data-mode", "darkmode")
+//      }
+// })
+
+
+mode.addEventListener('click', function () {
+     const ic_darkmode = document.querySelector(".tombol-darkmode .fas");
+     ic_darkmode.classList.toggle("dark-mode")
+     const roothtml = document.querySelector(".root");
+     roothtml.classList.toggle("root-dark-mode");
+
+});
